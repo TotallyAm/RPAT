0e491f9022d6cab1bdcdc5522f7ee3c00e24789f
unknown <totallyamp@gmail.com>
2025-08-06 22:11:51 +0100

    Fixed exiperimental branch

--- scripts/user_input.py
+++ scripts/user_input.py
@@ -1,19 +1,41 @@
 import json
 import os
+from scripts.ansi import *
+from config import DEFAULT_ROCKETS_PATH, CUSTOM_ROCKETS_PATH
+from dataclasses import dataclass
+from typing import List
 
+@dataclass
+class Rocket:
+    name: str
+    stages: int
+    isp: List[float]
+    dryMass: List[float]
+    wetMass: List[float]
+    dryMassAdj: List[float]
+    wetMassAdj: List[float]
+    fuel_reserve: List[float]
+    man_stage_addition: bool
+    rocketName: str
+    rocketMass: float
 
-
+def totalMass(man_stage_addition, wetMass):
+  if man_stage_addition:
+    rocketMass = max(wetMass)
+  else:
+    rocketMass = sum(wetMass)
+  return rocketMass
 
 def loadRockets():
   rockets = {}
   
   #default rockets
-  defaultPath = os.path.join(os.path.dirname(__file__), "default_rockets.json")
+  defaultPath = os.path.join(os.path.dirname(__file__), DEFAULT_ROCKETS_PATH)
   with open(defaultPath, "r") as f:
     rockets.update(json.load(f))
   
   #custom rockets
-  customPath = os.path.join(os.path.dirname(__file__), "custom_rockets.json")
+  customPath = os.path.join(os.path.dirname(__file__), CUSTOM_ROCKETS_PATH)
   if os.path.exists(customPath):
     with open(customPath, "r") as f:
       customPath = json.load(f)
@@ -41,7 +63,7 @@ def addReserves(stages, fuel_reserve, dryMass, wetMass):
       reserve = 0
     
     if reserve > 0:
-      print(f"Stage {i + 1} has a fuel reserve of {reserve} kg.")
+      #print(f"Stage {i + 1} has a fuel reserve of {reserve} kg.")
       dryMassAdj.append(dryMass[i] + reserve)
       wetMassAdj.append(wetMass[i] - reserve)
     
@@ -53,59 +75,66 @@ def addReserves(stages, fuel_reserve, dryMass, wetMass):
 
 
 def selectDefault():
-  rockets = loadRockets()
-  print("\nAvailable Default Rockets:")
-  for i, (key, rocket) in enumerate(rockets.items()):
-    print(f"{i:3}: {key:2} — {rocket['desc']}")
-  selected = input("\nEnter the rocket name or number: ").lower().strip()
-  
-  if selected.isdigit():
-    index = int(selected)
+    rockets = loadRockets()
+    print(YELLOW("\nAvailable Default Rockets:"))
+    for i, (key, rocket) in enumerate(rockets.items()):
+        print(f"{GRAY(f'{i:3}')} : {D_GRAY(key):2} — {GRAY(rocket['desc'])}")
     
-    if 0 <= index < len(rockets):
-      key = list(rockets.keys())[index]
+    selected = input(f"\n{GREEN('Enter the rocket name or number: ')}").lower().strip()
+    
+    if selected.isdigit():
+        index = int(selected)
+        
+        if 0 <= index < len(rockets):
+            key = list(rockets.keys())[index]
+        else:
+            print(RED("Invalid selection. Please try again."))
+            return None
+    
+    elif selected in rockets:
+        key = selected
     
     else:
-      print("Invalid selection. Please try again.")
-      return None
-  
-  elif selected in rockets:
-    key = selected
-  
-  else:
-    print("Rocket not found, please try again.")
+        print(RED("Rocket not found, please try again."))
+        return None
 
-  rocket = rockets[key]
-  
-  fuel_reserve = rocket.get("fuel_reserve", [0] * rocket["stages"])
-  
-  dryMassAdj, wetMassAdj = addReserves(rocket["stages"], fuel_reserve, rocket["dryMass"], rocket["wetMass"])
+    rocket = rockets[key]
 
-  print(f"Selected rocket: {rocket['desc']}")
-  
-  return (
-    rocket["stages"],
-    dryMassAdj,
-    wetMassAdj,
-    rocket["dryMass"],
-    rocket["wetMass"],
-    rocket["isp"],
-    rocket["manStage"],
-    rocket["desc"],
-  )
+    fuel_reserve = rocket.get("fuel_reserve", [0] * rocket["stages"])
+
+    dryMassAdj, wetMassAdj = addReserves(rocket["stages"], fuel_reserve, rocket["dryMass"], rocket["wetMass"])
+
+    rocketMass = totalMass(rocket["manStage"], wetMassAdj)
+
+    print(f"{GREEN('Selected rocket:')} {GRAY(rocket['desc'])}")
+
+    return Rocket(
+      name=key,
+      stages=rocket["stages"],
+      isp=rocket["isp"],
+      dryMass=rocket["dryMass"],
+      wetMass=rocket["wetMass"],
+      dryMassAdj=dryMassAdj,
+      wetMassAdj=wetMassAdj,
+      fuel_reserve=fuel_reserve,
+      man_stage_addition=rocket["manStage"],
+      rocketName=rocket["desc"],
+      rocketMass=rocketMass
+    )
+
   
 def manualEntry():
   while True:
-    name = input("What is the name of your rocket? ")
+    name = input(GREEN("What is the name of your rocket? "))
     break
     
   try:
-    stages = int(input("How many stages does your rocket have? "))
+    stages = int(input(GREEN("How many stages does your rocket have? ")))
   except ValueError: 
     print("Invalid input, please try again.")
 
   while True:
-    response = input("Will you be including the mass of the upper stages in the lower stages? (y/n): ").strip().lower()
+    response = input(GREEN("Will you be including the mass of the upper stages in the lower stages? (y/n): ")).strip().lower()
     
     if response in ("y", "yes", "true"):
         manStage = True
@@ -116,7 +145,7 @@ def manualEntry():
         break
     
     else:
-        print("Please enter 'y' or 'n'.")
+        print(GREEN("Please enter 'y' or 'n'."))
     
     
   dryMass = []
@@ -126,10 +155,10 @@ def manualEntry():
   isp     = []
 
   for i in range(stages):
-    print(f"\nStage {i+1}:")
-    dry  = float(input("  Dry mass (kg) : "))
-    wet  = float(input("  Wet Mass (kg) : "))
-    isps = float(input("  ISP (s)       : "))
+    print(YELLOW(f"\nStage {i+1}:"))
+    dry  = float(input(GREEN("  Dry mass (kg) : ")))
+    wet  = float(input(GREEN("  Wet Mass (kg) : ")))
+    isps = float(input(GREEN("  ISP (s)       : ")))
     dryMass.append(dry)
     wetMass.append(wet)
     isp.append(isps)
@@ -137,23 +166,29 @@ def manualEntry():
   fuel_reserve = [0] * stages
 
   dryMassAdj, wetMassAdj = addReserves(stages, fuel_reserve, dryMass, wetMass)
+
+  rocketMass = totalMass(manStage, wetMassAdj)
   
     
-  return (
-    stages, 
-    dryMassAdj,
-    wetMassAdj,
-    dryMass,
-    wetMass,
-    isp, 
-    manStage, 
-    name
+  return Rocket(
+      name=name,
+      stages=stages,
+      isp=isp,
+      dryMass=dryMass,
+      wetMass=wetMass,
+      dryMassAdj=dryMassAdj,
+      wetMassAdj=wetMassAdj,
+      fuel_reserve=fuel_reserve,
+      manual_stage_addition=manStage,
+      rocketName=name,
+      rocketMass=rocketMass
   )
+
 
 
 def getParam():
   while True:
-    response = input("Do you want to use a preset rocket? (y/n): ").strip().lower()
+    response = input(GREEN("\nDo you want to use a preset rocket? (y/n): ")).strip().lower()
     
     if response in ("y", "yes", "true"):
         return selectDefault()
@@ -164,3 +199,5 @@ def getParam():
     else:
         print("Please enter 'y' or 'n'.")
         continue
+    
+rocket = getParam()
